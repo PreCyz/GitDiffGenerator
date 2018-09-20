9584eb83ad6cab9a7db37596ad9035b1c57a2480
Pawel Gawedzki <pawg@netcompany.com>
2018-09-20 08:54:39 +0200

    Adding DiffProducerFactory.

---
+++ src/pg/gipter/producer/DiffProducerFactory.java
@@ -0,0 +1,23 @@
+package pg.gipter.producer;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+/**Created by Pawel Gawedzki on 20-Sep-2018.*/
+public final class DiffProducerFactory {
+
+    private DiffProducerFactory() { }
+
+    public static DiffProducer getInstance(String[] programArguments) {
+        String platform = System.getProperty("os.name");
+        System.out.printf("Running on platform [%s].%n", platform);
+
+        if ("Linux".equalsIgnoreCase(platform)) {
+            return new LinuxDiffProducer(programArguments);
+        } else if (platform.startsWith("Windows")) {
+            return new WindowsDiffProducer(programArguments);
+        } else {
+            System.err.printf("Platform %s not supported yet.%n", platform);
+            throw new NotImplementedException();
+        }
+    }
+}
