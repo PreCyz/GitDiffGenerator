ff674f5b77b5243898774fa03075fd0e06a4fa43
PreCyz <precpaw@op.pl>
2018-10-10 21:29:30 +0200

    Moving date formatter to Main class.

--- src/pg/gipter/Main.java
+++ src/pg/gipter/Main.java
@@ -3,8 +3,12 @@ package pg.gipter;
 import pg.gipter.producer.DiffProducer;
 import pg.gipter.producer.DiffProducerFactory;
 
+import java.time.format.DateTimeFormatter;
+
 /**Created by Pawel Gawedzki on 17-Sep-2018.*/
 public class Main {
+
+    public static final DateTimeFormatter yyyy_MM_dd = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     public static void main(String[] args) {
         DiffProducer diffProducer = DiffProducerFactory.getInstance(args);
