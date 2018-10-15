c22353f33619aec65e01858442167e304a495294
Pawel Gawedzki <pawg@netcompany.com>
2018-10-15 15:17:29 +0200

    Changing to custom http client.

--- src/main/java/pg/gipter/toolkit/sharepoint/SharePointRestClient.java
+++ src/main/java/pg/gipter/toolkit/sharepoint/SharePointRestClient.java
@@ -1,5 +1,6 @@
 package pg.gipter.toolkit.sharepoint;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHeaders;
@@ -11,11 +12,17 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicHeader;
 import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 /**Created by Pawel Gawedzki on 15-Oct-2018.*/
 public class SharePointRestClient {
+
+    private static final Logger logger = LoggerFactory.getLogger(SharePointRestClient.class);
 
     private final String toolkitUrl;
     private final String apiPrefix = "/_api/web/lists/";
@@ -30,23 +37,29 @@ public class SharePointRestClient {
         return String.format("GetByTitle('%s')", listName);
     }
 
+    /** Executing request under //https://goto.netcompany.com/cases/GTE106/NCSCOPY/_api/web/lists/GetByTitle('WorkItems')
+     *
+     */
     public void getListByTitle() {
-        /*Header header = new BasicHeader(HttpHeaders.CONTENT_TYPE, "application/json");
-        List<Header> headers = Lists.newArrayList(header);
-        HttpClient client = HttpClients.custom().setDefaultHeaders(headers).build();
-        HttpUriRequest request = RequestBuilder.get().setUri(SAMPLE_URL).build();
-        client.execute(request);*/
-
-        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
+        try (CloseableHttpClient httpclient = createHttpClient()) {
             HttpGet httpGet = new HttpGet(toolkitUrl + apiPrefix + getByTitle());
             Header[] headers = new Header[]{
-                    new BasicHeader(HttpHeaders.CONTENT_TYPE, "application/json")
+                    new BasicHeader(HttpHeaders.CONTENT_TYPE, "application/json;odata=verbose")
             };
             httpGet.setHeaders(headers);
             executeRequest(httpclient, httpGet);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private CloseableHttpClient createHttpClient() {
+        //Header header = new BasicHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+        //List<Header> headers = Arrays.asList(header);
+        return HttpClients
+                .custom()
+                //.setDefaultHeaders(headers)
+                .build();
     }
 
     private void executeRequest(CloseableHttpClient httpclient, HttpUriRequest httpRequest) throws IOException {
@@ -59,13 +72,27 @@ public class SharePointRestClient {
         }
     }
 
-    public void uploadFile(String filePath) {
-        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
+    //https://goto.netcompany.com/cases/GTE106/NCSCOPY/_api/web/getfolderbyserverrelativeurl('PAWG')/files/add(overwrite=true, url='fileName')",
+    public void uploadFile(String attachmentPath) {
+
+        byte[] attachment;
+        try (InputStream is = new FileInputStream(attachmentPath)) {
+
+            attachment = IOUtils.toByteArray(is);
+
+        } catch (IOException e) {
+            logger.error(e.getMessage(), e);
+            throw new RuntimeException();
+        }
+
+        try (CloseableHttpClient httpclient = createHttpClient()) {
             HttpPost httpPost = new HttpPost("http://targethost/login");
-            /*List<NameValuePair> nvps = new ArrayList<>();
-            nvps.add(new BasicNameValuePair("username", "vip"));
-            nvps.add(new BasicNameValuePair("password", "secret"));
-            httpPost.setEntity(new UrlEncodedFormEntity(nvps));*/
+            Header[] headers = new Header[]{
+                    new BasicHeader("accept", "application/json;odata=verbose"),
+                    new BasicHeader("X-RequestDigest", "#__REQUESTDIGEST!!!!!!!"),
+                    new BasicHeader("content-length", String.valueOf(attachment.length)),
+            };
+            httpPost.setHeaders(headers);
 
             executeRequest(httpclient, httpPost);
 
