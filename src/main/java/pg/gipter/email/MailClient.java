0097e5a4622a48bcddb4a56455b7923f90456c10
PreCyz <precpaw@op.pl>
2018-10-17 20:06:45 +0200

    Cleaning MailClient.

--- src/main/java/pg/gipter/email/MailClient.java
+++ src/main/java/pg/gipter/email/MailClient.java
@@ -1,9 +1,9 @@
 package pg.gipter.email;
 
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.util.Properties;
@@ -11,106 +11,101 @@ import java.util.Properties;
 /**Created by Pawel Gawedzki on 17-Oct-2018.*/
 public class MailClient {
 
+    private static final Logger logger = LoggerFactory.getLogger(MailClient.class);
+
     public static void main(String[] args) {
-        sendEmailSuccessMsg("pawg@netcompany.com");
+        new MailClient().sendEmailSuccessMsg("pgawedzki@gmail.com");
         System.exit(0);
     }
 
-    public static void sendEmailSuccessMsg(String recipient) {
+    private static final String FROM = "gipter@noresponse.com";
+    private static final String HOST = "mail.netcompany.com";
 
-        // Sender's email ID needs to be mentioned
-        String from = "gipter@netcompany.com";
+    private Properties emailTlsProperties() {
 
-        // Assuming you are sending email from localhost
-        String host = "mail.netcompany.com";
-
-        // Get system properties
-        Properties properties = System.getProperties();
-
+        /*
         // Setup mail server
         properties.put("mail.transport.protocol", "smtp");
         properties.setProperty("mail.smtp.host", host);
         properties.put("mail.smtp.port", "443");
         properties.put("mail.smtp.starttls.enable", "true");
         properties.put("mail.smtp.auth", "true");
-        /*properties.put("mail.smtp.socketFactory.port", "443");
+        properties.put("mail.smtp.socketFactory.port", "443");
         properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-        properties.put("mail.smtp.socketFactory.fallback", "false");*/
+        properties.put("mail.smtp.socketFactory.fallback", "false");
+*/
+        Properties properties = System.getProperties();
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.port", "587");
+        return properties;
+    }
 
-        //Authenticator auth = new SMTPAuthenticator();
+    private Properties emailSslProperties() {
+        Properties properties = System.getProperties();
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.socketFactory.port", "465");
+        properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.port", "465");
+        return properties;
+    }
 
-        // Get the default Session object.
-        Session session = Session.getDefaultInstance(properties);
-
-
+    public void sendEmailSuccessMsg(String recipient) {
         try {
-            // Create a default MimeMessage object.
-            MimeMessage message = new MimeMessage(session);
-
-            // Set From: header field of the header.
-            message.setFrom(new InternetAddress(from));
-
-            // Set To: header field of the header.
-            message.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
-
-            // Set Subject: header field
-            message.setSubject("This is the Subject Line!");
-
-            // Send the actual HTML message, as big as you like
-            message.setContent("<h1>Your </h1>", "text/html");
-
-            // Send message
-            Transport transport = session.getTransport();
-
-            transport.connect();
-            transport.sendMessage(message, message.getRecipients(Message.RecipientType.TO));
-            transport.close();
-
-            //Transport.send(message);
-            System.out.println("Sent message successfully....");
+            //Session session = Session.getDefaultInstance(properties);
+            Session session = newSession();
+            sendMessage(session, successMessage(recipient, session));
         } catch (MessagingException mex) {
-            mex.printStackTrace();
+            logger.error("Error when sending success email.", mex);
         }
     }
 
-    public static void sendEmailErrorMsg(String recipient) {
+    private void sendMessage(Session session, MimeMessage message) throws MessagingException {
+        Transport transport = session.getTransport();
 
-        // Sender's email ID needs to be mentioned
-        String from = "web@gmail.com";
+        transport.connect();
+        transport.sendMessage(message, message.getRecipients(Message.RecipientType.TO));
+        transport.close();
 
-        // Assuming you are sending email from localhost
-        String host = "localhost";
+        //Transport.send(message);
+        System.out.println("Sent message successfully....");
+    }
 
-        // Get system properties
-        Properties properties = System.getProperties();
+    private Session newSession() {
+        return Session.getInstance(emailSslProperties(),
+                    new Authenticator() {
+                        protected PasswordAuthentication getPasswordAuthentication() {
+                            return new PasswordAuthentication("userName", "password");
+                        }
+                    });
+    }
 
-        // Setup mail server
-        properties.setProperty("mail.smtp.host", host);
+    private MimeMessage successMessage(String recipient, Session session) throws MessagingException {
+        MimeMessage message = new MimeMessage(session);
+        message.setFrom(new InternetAddress(FROM));
+        message.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
+        message.setSubject("This is the Subject Line!");
+        message.setContent("<h1>Your </h1>", "text/html");
+        return message;
+    }
 
-        // Get the default Session object.
-        Session session = Session.getDefaultInstance(properties);
+    private MimeMessage errorMessage(String recipient, Session session) throws MessagingException {
+        MimeMessage message = new MimeMessage(session);
+        message.setFrom(new InternetAddress(FROM));
+        message.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
+        message.setSubject("Error!");
+        message.setContent("<h1>Your </h1>", "text/html");
+        return message;
+    }
 
+    public void sendEmailErrorMsg(String recipient) {
         try {
-            // Create a default MimeMessage object.
-            MimeMessage message = new MimeMessage(session);
-
-            // Set From: header field of the header.
-            message.setFrom(new InternetAddress(from));
-
-            // Set To: header field of the header.
-            message.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
-
-            // Set Subject: header field
-            message.setSubject("This is the Subject Line!");
-
-            // Send the actual HTML message, as big as you like
-            message.setContent("<h1>Your </h1>", "text/html");
-
-            // Send message
-            Transport.send(message);
-            System.out.println("Sent message successfully....");
+            Session session = newSession();
+            sendMessage(session, errorMessage(recipient, session));
         } catch (MessagingException mex) {
-            mex.printStackTrace();
+            logger.error("Error when sending error email.", mex);
         }
     }
 }
