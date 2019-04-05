d13ae82e6cded21bffbfbcf38cca3186e3b54c3b
Pawel Gawedzki <pawg@netcompany.com>
2019-04-05 12:50:37 +0200

    Downloading document from toolkit.

--- src/test/java/GetDocumentVersion.java
+++ src/test/java/GetDocumentVersion.java
@@ -1,3 +1,13 @@
+import org.apache.commons.io.FileUtils;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.NTCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -18,6 +28,7 @@ import pg.gipter.toolkit.ws.GetVersionCollection;
 import pg.gipter.toolkit.ws.GetVersionCollectionResponse;
 import pg.gipter.toolkit.ws.ObjectFactory;
 
+import java.io.File;
 import java.util.Properties;
 
 /**
@@ -93,5 +104,32 @@ public class GetDocumentVersion {
         }
         //logger.error("Weird response from toolkit. Response is not a xml.");
         //throw new IllegalArgumentException("Weird response from toolkit. Response is not a xml.");*/
+    }
+
+    @Test
+    void download() throws Exception {
+        CredentialsProvider credsProvider = new BasicCredentialsProvider();
+        credsProvider.setCredentials(
+                new AuthScope(AuthScope.ANY),
+                new NTCredentials("pawg", "", "https://goto.netcompany.com", "NCDMZ"));
+        CloseableHttpClient httpclient = HttpClients.custom()
+                .setDefaultCredentialsProvider(credsProvider)
+                .build();
+        try {
+            HttpGet httpget = new HttpGet("https://goto.netcompany.com/cases/GTE440/TOEDNLD/_vti_history/14848/Deliverables/D0180%20-%20Integration%20design/Topdanmark%20integrations/D0180%20-%20Integration%20Design%20-%20Topdanmark%20integrations%20-%20Party%20Master.docx");
+
+            System.out.println("Executing request " + httpget.getRequestLine());
+            CloseableHttpResponse response = httpclient.execute(httpget);
+            try {
+                System.out.println("----------------------------------------");
+                System.out.println(response.getStatusLine());
+                FileUtils.copyInputStreamToFile(response.getEntity().getContent(), new File("tmp.docx"));
+                EntityUtils.consume(response.getEntity());
+            } finally {
+                response.close();
+            }
+        } finally {
+            httpclient.close();
+        }
     }
 }
