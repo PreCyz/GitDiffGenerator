ecc0bbd72c0f43c7fa4cf040dd5be1a2f1e84934
Pawel Gawedzki <pawg@netcompany.com>
2019-09-16 15:27:07 +0200

    Introducing SystemUtils class.

---
+++ src/main/java/pg/gipter/utils/SystemUtils.java
@@ -0,0 +1,27 @@
+package pg.gipter.utils;
+
+/** Created by Pawel Gawedzki on 16-Sep-2019. */
+public final class SystemUtils {
+
+    private SystemUtils() { }
+
+    public static final String OS = System.getProperty("os.name").toLowerCase();
+    private static final String OS_LOWER_CASE = OS.toLowerCase();
+
+    public static boolean isWindows() {
+        return OS_LOWER_CASE.contains("win");
+    }
+
+    public static boolean isMac() {
+        return OS_LOWER_CASE.contains("mac");
+    }
+
+    public static boolean isUnix() {
+        return OS_LOWER_CASE.contains("nix") || OS.contains("nux") || OS.contains("aix");
+    }
+
+    public static boolean isSolaris() {
+        return (OS_LOWER_CASE.contains("sunos"));
+    }
+
+}
