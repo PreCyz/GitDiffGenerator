a9f821b6a29a0e85be7ee27135634cda386f9e81
Pawel Gawedzki <pawg@netcompany.com>
2019-09-16 14:53:16 +0200

    Adding support for macs.

--- src/main/java/pg/gipter/producer/DiffProducerFactory.java
+++ src/main/java/pg/gipter/producer/DiffProducerFactory.java
@@ -9,23 +9,41 @@ import pg.gipter.settings.ApplicationProperties;
 public final class DiffProducerFactory {
 
     private static final Logger logger = LoggerFactory.getLogger(DiffProducerFactory.class);
+    private static final String OS = System.getProperty("os.name").toLowerCase();
 
     private DiffProducerFactory() { }
 
     public static DiffProducer getInstance(ApplicationProperties applicationProperties) {
-        String platform = System.getProperty("os.name");
-        logger.info("Running on platform [{}].", platform);
+        logger.info("Running on platform [{}].", OS);
         if (applicationProperties.uploadType() == UploadType.STATEMENT) {
             return new StatementDiffProducer(applicationProperties);
         } else if (applicationProperties.uploadType() == UploadType.TOOLKIT_DOCS) {
             return new ToolkitDocumentsDiffProducer(applicationProperties);
-        } else if ("Linux".equalsIgnoreCase(platform)) {
+        } else if (isUnix()) {
             return new LinuxDiffProducer(applicationProperties);
-        } else if (platform.startsWith("Windows")) {
+        } else if (isWindows()) {
             return new WindowsDiffProducer(applicationProperties);
+        } else if (isMac()){
+            return new MacDiffProducer(applicationProperties);
         } else {
-            logger.warn("Platform {} not supported yet.", platform);
+            logger.warn("Platform {} not supported yet.", OS);
             throw new RuntimeException("Not implemented yet!!!");
         }
+    }
+
+    private static boolean isWindows() {
+        return OS.contains("win");
+    }
+
+    private static boolean isMac() {
+        return OS.contains("mac");
+    }
+
+    private static boolean isUnix() {
+        return OS.contains("nix") || OS.contains("nux") || OS.contains("aix");
+    }
+
+    private static boolean isSolaris() {
+        return (OS.contains("sunos"));
     }
 }
