f5668e048e641d6b3ba07fac97612a2d208f61cd
PreCyz <precpaw@op.pl>
2020-02-18 20:06:39 +0100

    Bringing back proper git command.

--- src/main/java/pg/gipter/core/producer/WindowsDiffProducer.java
+++ src/main/java/pg/gipter/core/producer/WindowsDiffProducer.java
@@ -2,7 +2,6 @@ package pg.gipter.core.producer;
 
 import pg.gipter.core.ApplicationProperties;
 
-import java.util.ArrayList;
 import java.util.List;
 
 class WindowsDiffProducer extends AbstractDiffProducer {
@@ -13,10 +12,6 @@ class WindowsDiffProducer extends AbstractDiffProducer {
 
     @Override
     protected List<String> getFullCommand(List<String> diffCmd) {
-        ArrayList<String> command = new ArrayList<>(diffCmd.size() + 2);
-        command.add("powershell.exe");
-        command.add("-Command");
-        command.addAll(diffCmd);
-        return command;
+        return diffCmd;
     }
 }
