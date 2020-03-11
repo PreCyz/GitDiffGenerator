b14724ad2f67146aa6383898082b7f95edd05422
PreCyz <precpaw@op.pl>
2020-03-11 12:17:56 +0100

    Adding new gifs.

--- src/main/java/pg/gipter/ui/alert/ImageFile.java
+++ src/main/java/pg/gipter/ui/alert/ImageFile.java
@@ -22,6 +22,12 @@ public enum ImageFile {
     MINION_IOIO_GIF("gif/minion-ioio.gif"),
     MINION_APPLAUSE_GIF("gif/minion-applause.gif"),
     MINION_CROWD_GIF("gif/minion-crowd.gif"),
+    BABY_YODA_GIF("gif/baby-yoda.gif"),
+    FIRE_BACK_GIF("gif/fire-back.gif"),
+    MCGONAGAL_GIF("gif/mcgonagal.gif"),
+    SNAPE_GIF("gif/snape.gif"),
+    KYLO_REN_PUSH_GIF("gif/kylo_ren_push.gif"),
+    KYLO_APPROVES_GIF("gif/kylo_approves.gif"),
     ;
 
     private String fileName;
@@ -41,14 +47,21 @@ public enum ImageFile {
     }
 
     public static ImageFile randomPartialSuccessImage() {
-        return randomImage(EnumSet.of(ALMOST_ALL_PNG, MINION_BOMB_GIF, MINION_FART_GIF, MINION_CROWD_GIF));
+        return randomImage(EnumSet.of(
+                ALMOST_ALL_PNG, MINION_BOMB_GIF, MINION_FART_GIF, MINION_CROWD_GIF, BABY_YODA_GIF
+        ));
     }
 
     public static ImageFile randomSuccessImage() {
-        return randomImage(EnumSet.of(GOOD_JOB_PNG, MINION_AAAA_GIF, MINION_DISCO_GIF, MINION_AAAA_2_GIF, MINION_APPLAUSE_GIF));
+        return randomImage(EnumSet.of(
+                GOOD_JOB_PNG, MINION_AAAA_GIF, MINION_DISCO_GIF, MINION_AAAA_2_GIF, MINION_APPLAUSE_GIF, MCGONAGAL_GIF,
+                KYLO_APPROVES_GIF
+        ));
     }
 
     public static ImageFile randomFailImage() {
-        return randomImage(EnumSet.of(ERROR_CHICKEN_PNG, MINION_IOIO_GIF));
+        return randomImage(EnumSet.of(
+                ERROR_CHICKEN_PNG, MINION_IOIO_GIF, FIRE_BACK_GIF, SNAPE_GIF, KYLO_REN_PUSH_GIF
+        ));
     }
 }
