d7b7f48ea3bdfd7c40bb334f922d5c577bc197ab
Precyzik <gawaprecyz@gmail.com>
2025-10-05 20:54:36 +0200

    Fixing decompress script.

--- src/main/java/pg/gipter/services/UpgradeService.java
+++ src/main/java/pg/gipter/services/UpgradeService.java
@@ -87,6 +87,10 @@ public class UpgradeService extends TaskService<Void> {
             SevenZArchiveEntry entry;
             while ((entry = sevenZFile.getNextEntry()) != null) {
                 if (entry.isDirectory()) {
+                    Path directory = Paths.get(destination.getName(), entry.getName());
+                    if (!Files.exists(directory)) {
+                        Files.createDirectory(directory);
+                    }
                     continue;
                 }
                 File currentFile = new File(destination, entry.getName());
